68d3a4b46b33c39f3e179034ccbf3e7b012cd5a8
umarnaxir <erumarnazir@gmail.com>
2025-08-20 00:12:58 +0530

    sitemap added

--- app/page.js
+++ app/page.js
@@ -1,27 +1,50 @@
-import Navbar from "@/app/components/Navbar";
-import Hero from "@/app/components/Hero";
-import About from "@/app/components/About";
-import Skills from "@/app/components/Skills";
-import Experience from "@/app/components/Experience";
-import Projects from "@/app/components/Projects";
-import Contact from "@/app/components/Contact";
-import Footer from "@/app/components/Footer"
-import Education from "./components/Education";
-import BackToTop from "./components/BackToTop";
+import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
 
-export default function Home() {
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Umar Nazir | Software Engineer & SEO Executive",
+  description:
+    "Explore the professional portfolio of Umar Nazir – a software Engineer and SEO Executive with expertise in React, Next.js, Tailwind CSS, digital marketing, and graphic design.",
+  icons: {
+    icon: "/favicon.png",
+  },
+  verification: {
+    google: "K5WPaPu_n40Lp7BlSC2vph3oTrM3QzSlCbkCSZpA2iE", //Search Console verification
+  },
+};
+
+export default function RootLayout({ children }) {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Education/>
-      <Skills />
-      <Experience />
-      <Contact />
-      <Footer/>
-      <BackToTop/>
-    </>
+    <html lang="en">
+      <head>
+        {/*Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JCFC319Y7Q"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JCFC319Y7Q');
+          `}
+        </Script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
   );
 }
